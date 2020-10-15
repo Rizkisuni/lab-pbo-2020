6ac7a9bf452a0cab9c978539d1b6ccd9b23d4721
Rizkisuni <sunialfarisi@gmail.com>
2020-10-16 01:10:25 +0700

    Membuat Kode pertemuan dua

---
+++ pertemuan 2/latihan4.java
@@ -0,0 +1,10 @@
+/**
+* Program berikut untuk mendapatkan nilai argumen pada method main
+*/
+public class latihan4 {
+public static void main(String args[]) {
+System.out.println("Tanggal : " + args[0]);
+System.out.println("Bulan : " + args[1]);
+System.out.println("Tahun : " + args[2]);
+}
+}
